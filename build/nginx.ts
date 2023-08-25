d145fb26b8caac49a5f220e8d444078fb6ba8314
xlearns <2447684095@qq.com>
2023-08-25 17:53:42 +0800

    feat: add generate nginx

---
+++ build/nginx.ts
@@ -0,0 +1,61 @@
+import { writeFile, readFileSync, unlinkSync, existsSync } from "fs";
+import { resolve } from "path";
+import dotenv from "dotenv";
+const root = process.cwd();
+
+function createNginxConfig(config) {
+  const str = `
+  server {
+    listen       ${config["VITE_DEPLOYMENT_PORT"]};
+    server_name  localhost;
+    error_page  405     =200 $uri;
+    
+    location / {
+      root   /usr/share/nginx/html;
+      try_files  $uri $uri/ @router;
+      autoindex on;       
+      autoindex_exact_size off;   
+      charset utf-8;        
+      proxy_set_header X-Real-IP $remote_addr;
+      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+      proxy_set_header X-Forwarded-Proto $scheme;
+      index  index.html index.htm;
+    }
+${JSON.parse(config["VITE_PROXY"])
+  .map(([key, val]) => {
+    return `
+    location ${key}{
+      rewrite ^${key}/(.*)$ /$1 break;
+      proxy_pass  ${val};
+    }
+    `;
+  })
+  .join(" ")}
+    location @router{
+      rewrite ^.*$ /index.html last;
+    }
+  }
+  `;
+  return str;
+}
+
+async function markConf(config) {
+  const output = resolve(root, "nginx.conf");
+  const data = createNginxConfig(config);
+  await writeFile(output, data, () => {
+    //
+  });
+}
+
+function init() {
+  const nginxURL = resolve(root, "nginx.conf");
+  const configUrl = resolve(root, ".env.production");
+  if (existsSync(nginxURL)) {
+    unlinkSync(nginxURL);
+  }
+  const envContent = readFileSync(configUrl, "utf-8");
+  const envConfig = dotenv.parse(envContent);
+  markConf(envConfig);
+}
+
+init();
--- ci/docker/nginx.dockerfile
+++ ci/docker/nginx.dockerfile
@@ -3,6 +3,8 @@ FROM node:20-alpine as build-stage
 
 WORKDIR /app
 
+COPY package*.json ./
+
 RUN npm install
 
 COPY . .
@@ -13,7 +15,7 @@ RUN npm run build
 FROM nginx:1.21-alpine as production-stage
 
 COPY --from=build-stage /app/dist /usr/share/nginx/html
-
+COPY --from=build-stage /app/nginx.config  /etc/nginx/conf.d/default.conf
 EXPOSE 80
 
 CMD ["nginx", "-g", "daemon off;"]
